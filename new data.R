54ef55f3a923bcde2d25b50e373227e9a7751ca8
gurbakhsh9620 <gurbakhsh9620@gmail.com>
2021-06-09 20:21:20 +0530

    first commit

---
+++ new data.R
@@ -0,0 +1,90 @@
+a=1
+
+num1=10
+num2=20
+num1+num2
+num1*num2
+num1/num2
+num1<num2
+a=1
+a=1
+arithmetic operators
+num1 = 10
+num2 = 20
+num1+num2
+num1-num2
+num1*num2
+num1/num2
+relational operators
+num1>num2
+num1>num2
+
+num1
+b=20
+
+
+
+1+2
+sqrt(64) for squareroot
+sqrt(96)
+#exponents
+
+10^6
+2^2
+34*69
+8/2
+
+?sqrt
+?mean
+
+mean(1,10,100)
+mean(c(1,10,100))
+a <- c(1,10,100)
+mean(a)
+sum(a)
+median(a)
+max(a)
+min(a)
+
+a=1
+a
+str(a)
+class(a)
+
+b = "dog"
+b
+str(b)
+class(b)
+
+
+saving a string
+a = "x1"
+a
+str(a)
+class(a)
+
+
+a <- 1:10
+a
+str(a)
+class(a)
+
+#assign new value b
+a <- c(34,5.8,6.9,4.5)
+a [c(3,4)]
+a [c(1,2)]
+
+as.integer(c(34,5.8,6.9,4.5))
+round(c(34,5.8,9.4,4.5))
+
+b <- c("1.7","2.8","3.8","4.5")
+sum(b)
+sum(as.numeric(b))
+round(as.numeric(b))
+is.numeric(b)
+is.character(b)
+N <- as.numeric(b)
+is.numeric(b)
+
+is.character(b)
+
